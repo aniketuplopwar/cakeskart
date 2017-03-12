f47dca0c802d91f4719e83bb5bf5ffffe1ddec2b
Aniket <aniket.uplopwar@gmail.com>
2017-03-12 18:56:19 +0530

    adding  major build fix

--- server/app.js
+++ server/app.js
@@ -13,12 +13,14 @@ app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '../src/views'));
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../public/images/favicon.jpg')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+//app.use('/favicon.ico', express.static('../public/images/favicon.jpeg'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
